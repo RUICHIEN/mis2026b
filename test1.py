4389bbf9ef91aaac3c4902dfd8b3cfb7885e7c3b
RUICHIEN <s1135196@pu.edu.tw>
2026-03-12 11:54:38 +0800

    20260312
    python練習，包含作業內容。

---
+++ test1.py
@@ -0,0 +1,14 @@
+def square(y):
+  print(f"{y} 的平方為 {y*y}")
+
+
+x=int(input('請輸入:'))
+#x+=10
+#print('輸入:',x)
+if (x<=0):
+	print(f'您輸入的數字{x}小魚等魚0')
+else:
+	print(f'您輸入的數字{x}大於0')
+	for i in range(1,x+1):
+		#print(i,end="; ")
+		square(i)
---
+++ test2.py
@@ -0,0 +1,8 @@
+def life(y):
+  print(f"{}")
+
+
+x=int(input('請輸入西元生日(年月日):'))
+print(x[0])
+#西元年月日8位相加
+#練習for迴圈
